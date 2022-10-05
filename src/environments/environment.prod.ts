b978cccd85945e17bebf5e27dc24a7a0f3bfe6bb
José Luis Barrientos Terán <jbarrientos2@tenaris.com>
2022-10-04 20:05:43 -0500

    fix production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCIPjJDuGMT_fCxahfzU6DCFtvmu2VNQ1w",
+    authDomain: "tema14-storage.firebaseapp.com",
+    projectId: "tema14-storage",
+    storageBucket: "tema14-storage.appspot.com",
+    messagingSenderId: "871692896246",
+    appId: "1:871692896246:web:a4505d7ec56fb6c443a9d7"
+  }
 };
